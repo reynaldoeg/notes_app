cc3905db55422be3b331ed1d4ac24646003c3459
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2019-09-12 02:46:13 +0000

    Fix: folders rel

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,7 @@
 class Folder < ApplicationRecord
   
-  #belongs_to :parent, :class_name => "Folder"
-  #has_many :folders, :foreign_key => "parent_id"
+  belongs_to :parent, :class_name => "Folder"
+  has_many :folders, :foreign_key => "parent_id"
   
   validates :name, presence: true
 
