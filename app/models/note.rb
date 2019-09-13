960865ccbe96dab243876f6a9902720500dde7a7
Reynaldo Esparza <reynaldoeg_esparza@hotmail.com>
2019-09-13 20:39:04 +0000

    Formato de notas

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,5 @@ class Note < ApplicationRecord
   belongs_to :folder
   
   validates :name, presence: true
-  validates :content, length: { maximum: 1024 }, presence: true
+  validates :content, presence: true
 end
